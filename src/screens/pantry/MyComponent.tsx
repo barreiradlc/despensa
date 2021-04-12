1514d831a3f2c830995de8375866a09ed3d1ff74
Barreira <barreira266@hotmail.com>
2021-04-11 22:45:39 -0300

    Salvamento local e Renderização de despensas da nuvem

--- src/screens/pantry/MyComponent.tsx
+++ src/screens/pantry/MyComponent.tsx
@@ -1,33 +1,44 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+
+
 import { List } from 'react-native-paper';
 import { Pantry } from '../../services/local/realm/PantryLocalService';
+import React, { useState, useEffect } from "react";
+import Tooltip from "react-native-walkthrough-tooltip";
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Platform, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TooltipComponent from '../../components/utils/TooltipComponent';
+import ListAccordionGroup from 'react-native-paper/lib/typescript/components/List/ListAccordionGroup';
 
-interface MyComponentInterface{
-    pantries: Pantry[]
+
+interface MyComponentInterface {
+  pantries: Pantry[]
 }
 
 const MyComponent = ({ pantries }: MyComponentInterface) => {
-  const [expanded, setExpanded] = React.useState(true);
-
+  const [expanded, setExpanded] = React.useState(false);
   const handlePress = () => setExpanded(!expanded);
 
   return (
-    // <List.Section title="Accordions">              
-    <List.Section>              
+    <List.AccordionGroup>
+
+      {!!pantries.length && pantries.map((pantry: Pantry) => (
+        <List.Accordion
         
-        {pantries.map((pantry) => (
-            <List.Accordion
-                title={pantry.name}
-                description={pantry.description}
-                left={props => <List.Icon {...props} icon="folder" />}
-                expanded={expanded}
-                onPress={handlePress}>
-                <List.Item title="First item" />
-                <List.Item title="Second item" />
-            </List.Accordion>
-        ))}
-    </List.Section>
+          key={pantry.uuid}
+          id={pantry.uuid}
+          title={pantry.name}
+          description={pantry.description}
+          left={props => <List.Icon {...props} icon="folder" />}
+          expanded={expanded}
+          onPress={handlePress}>
+          <List.Item title="First item" />
+          <List.Item title="Second item" />
+        </List.Accordion>
+
+      ))}
+
+    </List.AccordionGroup>
   );
 };
 
