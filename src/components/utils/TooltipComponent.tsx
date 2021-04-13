e80c7383f83d021805b6c756832398d584452a55
Barreira <barreira266@hotmail.com>
2021-04-12 21:51:52 -0300

    Tooltip Corrigido

--- src/components/utils/TooltipComponent.tsx
+++ src/components/utils/TooltipComponent.tsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { View, Text, Alert } from 'react-native';
 import Tooltip from "react-native-walkthrough-tooltip";
 
 interface ToolipInterface {
-    content?: any;
-    component: any;
+  content?: any;
+  component?: any;
 }
 
-const TooltipComponent: React.FC<ToolipInterface> = ({ content, component }) => {
-    const [showTip, setTip] = useState(false);
+const TooltipComponent: React.FC<ToolipInterface> = ({ content, children }, ref) => {
+  const [showTip, setTip] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    toggleTooltip: () => {
+      setTip((prevData: boolean) => !prevData)
+    }
+  }));
+
+  // if(!showTip) return null
 
   return (
     <Tooltip
-    isVisible={showTip}
-    content={
-    content ||
-      <View>
-        <Text> Hello i am tooltip for below! I get shown on mount of second screen. </Text>
-      </View>
-    }
-    placement="top"
-    onClose={() => setTip(false)}
-    useInteractionManager={true} // need this prop to wait for react navigation
+      isVisible={showTip}
+      content={
+        content ||
+        <View>
+          <Text> Editar </Text>
+          <Text> Deletar </Text>
+          <Text> Cancelar </Text>
+        </View>
+      }
+      // contentStyle={{ position: 'absolute', top: 20 }}
+      placement='bottom'
+      onClose={() => setTip(false)}
+      // useInteractionManager={true} // need this prop to wait for react navigation
     // below is for the status bar of react navigation bar
     // topAdjustment={Platform.OS === 'android' ? -StatusBar.currentHeight : 0}
-  >
-      {component}
+    >
+      {children}
     </Tooltip>
   );
 }
 
-export default TooltipComponent;
+export default  forwardRef(TooltipComponent);
--- src/screens/pantry/MyComponent.tsx
+++ src/screens/pantry/MyComponent.tsx
@@ -2,40 +2,50 @@
 
 import { List } from 'react-native-paper';
 import { Pantry } from '../../services/local/realm/PantryLocalService';
-import React, { useState, useEffect } from "react";
-import Tooltip from "react-native-walkthrough-tooltip";
+import React, { useState, useEffect, useRef, createRef, RefObject, MutableRefObject, useMemo } from "react";
 import { Text, View, StyleSheet, TextInput, TouchableOpacity, Platform, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TooltipComponent from '../../components/utils/TooltipComponent';
 import ListAccordionGroup from 'react-native-paper/lib/typescript/components/List/ListAccordionGroup';
-
+import { Button } from '../../components/styles/form';
 
 interface MyComponentInterface {
   pantries: Pantry[]
 }
+interface TipRefInterface extends MutableRefObject<any | undefined> {
+  toggleTooltip(): void
+}
 
 const MyComponent = ({ pantries }: MyComponentInterface) => {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const pantriesRefs = useMemo(() => Array(pantries.length).fill().map(() => createRef()), [pantries] )
   const handlePress = () => setExpanded(!expanded);
 
   return (
     <List.AccordionGroup>
 
-      {!!pantries.length && pantries.map((pantry: Pantry) => (
+      {!!pantries.length && pantries.map((pantry: Pantry, index: number) => (
         <List.Accordion
-        
           key={pantry.uuid}
           id={pantry.uuid}
           title={pantry.name}
           description={pantry.description}
-          left={props => <List.Icon {...props} icon="folder" />}
+          onLongPress={() => {
+            pantriesRefs[index]?.current.toggleTooltip()
+            console.log(pantriesRefs)
+          }}
+          titleStyle={{ color: "#C72828" }}
+          left={props =>
+            <TooltipComponent ref={pantriesRefs[index]}>
+              <List.Icon {...props} icon="folder" />
+            </TooltipComponent>
+          }
           expanded={expanded}
           onPress={handlePress}>
           <List.Item title="First item" />
           <List.Item title="Second item" />
         </List.Accordion>
-
       ))}
 
     </List.AccordionGroup>
